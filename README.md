b65d7dc6912c1a3183322e4ce945baa70aebc637
Awraam Deligiannis <awraam.deligiannis@deutschebahn.com>
2018-09-14 14:56:03 +0200

    First draft of chapter variables

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# java-workshop
+# Java-Workshop
 
---
+++ src/main/java/de/db/vertrieb/javaworkshop/VariablenTask1Main.java
@@ -0,0 +1,11 @@
+package de.db.vertrieb.javaworkshop;
+
+public class VariablenTask1Main {
+    public static void main(String[] args) {
+        int firstNumber = 15;
+        int secondNumber = 10;
+        int result = firstNumber + secondNumber;
+
+        System.out.print("Result: " + result);
+    }
+}
